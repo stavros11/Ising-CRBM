cacd209c2952b801eea74fbbae260eace3b64cf1
stavros11 <stavros11@users.noreply.github.com>
2018-07-14 17:16:49 +0300

    First upload

---
+++ crbm.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 11 14:39:14 2018
+
+@author: Stavros
+"""
+
+import tensorflow as tf
+
+class ConvRBM():
+    def __init__(self, Nv, Nw, K):
+        # sizes object has: Nv, Nw, K
+        self.Nv, self.Nw, self.K = Nv, Nw, K
+        self.Nh = self.Nv - self.Nw + 1
+        
+        self.visible = tf.placeholder(tf.float32)
+        self.hidden  = tf.placeholder(tf.float32)
+        
+        # Initialization according to Hinton Sec. 8
+        self.filter = tf.Variable(
+                tf.random_normal(shape=(self.Nw, self.Nw, 1, self.K), 
+                                 stddev=0.01), dtype=tf.float32)
+        
+        self.hid_bias = tf.Variable(tf.zeros(shape=(self.K,)), dtype=tf.float32)
+        self.vis_bias = tf.Variable(tf.zeros(shape=(1,)), dtype=tf.float32)
+    
+    def prob_given_v(self, v):
+        x = tf.nn.conv2d(v, self.filter, strides=(1,1,1,1), padding="VALID")
+        return tf.sigmoid(tf.add(x, self.hid_bias))
+    
+    def prob_given_h(self, h):
+        batch_size = tf.shape(h)[0]
+        x = tf.nn.conv2d_transpose(h, self.filter, strides=(1,1,1,1), padding="VALID",
+                                   output_shape=(batch_size, self.Nv, 
+                                                 self.Nv, 1))
+        return tf.sigmoid(tf.add(x, self.vis_bias))
+    
+    def create_gibbs_sampler(self, k=2):
+        # Returns graph for Gibbs sampling starting from a random v.
+        # k: number of block samplings
+        v = self.visible
+        for i in range(k):
+            h = self.sample_tensor(self.prob_given_v(v))
+            v = self.sample_tensor(self.prob_given_h(h))
+        return v
+    
+    def calculate_energy(self, v, h):
+        c = tf.nn.conv2d(v, self.filter, strides=(1,1,1,1), padding="VALID")
+        t1 = tf.reduce_sum(tf.multiply(h, c), axis=(1,2,3))
+        t2 = tf.reduce_sum(tf.multiply(self.hid_bias, 
+                                         tf.reduce_sum(h, axis=(1,2))), axis=1)
+        t3 = tf.multiply(self.vis_bias, tf.reduce_sum(v, axis=(1,2,3)))
+        
+        return -tf.add(t1, tf.add(t2, t3))
+    
+    def loss_for_grad(self, v, k=2):
+        # sample hidden from data
+        h_data = tf.stop_gradient(self.sample_tensor(self.prob_given_v(v)))
+        data_term = self.calculate_energy(v, h_data)
+        
+        # CD-k
+        h_recon = tf.stop_gradient(self.sample_tensor(self.prob_given_v(v)))
+        v_recon = self.sample_tensor(self.prob_given_h(h_recon))
+        for i in range(1, k):
+            h_recon = self.sample_tensor(self.prob_given_v(v_recon))
+            v_recon = self.sample_tensor(self.prob_given_h(h_recon))
+        h_recon = self.prob_given_v(v_recon)
+        recon_term = self.calculate_energy(v_recon, h_recon)
+        
+        return tf.reduce_mean(tf.subtract(data_term, recon_term))
+    
+    def prepare_training(self, k=2, learning_rate=0.01):
+        loss = self.loss_for_grad(v=self.visible, k=k)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+        self.train = optimizer.minimize(loss)
+        
+    def free_energy(self, v):
+        x = tf.nn.conv2d(v, self.filter, strides=(1,1,1,1), padding="VALID")
+        x = tf.add(tf.reduce_sum(x, axis=-1), tf.reduce_sum(self.hid_bias))
+        t = tf.multiply(self.vis_bias, tf.reduce_sum(v, axis=(1,2,3)))
+        return -tf.add(t, tf.reduce_sum(tf.nn.softplus(x), axis=(1,2)))
+    
+    def mean_free_energy(self):
+        ## Creates a graph for free energy calculation
+        return tf.reduce_mean(self.free_energy(self.visible))
+        
+    @staticmethod
+    def sample_tensor(prob):
+        # Returns binary tensor according to probability distribution prob
+        shape = tf.shape(prob)
+        return tf.where(
+            tf.less(tf.random_uniform(shape=shape), prob),
+            tf.ones(shape=shape),
+            tf.zeros(shape=shape))
+    
+#Nv, Nw, K = 8, 4, 10
+#n_samples = 10000
+#batch_size = 1000
+#epochs = 30
+#n_batches = n_samples // batch_size
+#
+#crbm = ConvRBM(Nv=Nv, Nw=Nw, K=K)
+#
+#data = np.ones((n_samples, Nv, Nv, 1))
+#rbm = ConvRBM(Nv, Nw, K)
+#rbm.prepare_training(k=10)
+#v_gibbs = rbm.create_gibbs_sampler(k=4)
+#
+#sess = tf.Session()
+#sess.run(tf.global_variables_initializer())
+#for iE in range(epochs):
+#    for iB in range(n_batches):
+#        sess.run(rbm.train, 
+#                 feed_dict={rbm.visible : data[iB * batch_size : (iB+1) * batch_size]})
+#        
+#    print('%d / %d epochs done!'%(iE+1, epochs))
+#    
+## Test
+#v_test = sess.run(v_gibbs, feed_dict={rbm.visible : np.random.randint(
+#        0, 2, size=(5000, Nv, Nv, 1))})
---
+++ ising.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 14 15:13:40 2018
+
+@author: Stavros
+"""
+
+##################################################################
+############ Functions that calculate quantities for     #########
+############ the 2D Ising model. Uses numpy (not keras)  #########
+##################################################################
+
+import numpy as np
+from correlation import correlation_lengths, two_point_function
+
+def get_basic_observables(state, T):
+    ## Returns [Mag, En, Susc, specHeat]
+    obj = Ising(2 * state - 1)
+    obj.calculate_moments()
+    
+    specHeat = (obj.energy2 - np.square(obj.energy)) / T**2
+    susc = (obj.mag2 - np.square(obj.mag)) / T
+    
+    return np.array([obj.mag, obj.energy, susc, specHeat]) / obj.N_spins
+
+def get_observables(state, T):
+    ## Returns [Mag, En, Susc, specHeat, Mag2, Mag4, En2]
+    return get_observables_assist(2*state-1, T)
+    
+def get_observables_assist(state, T):
+    obj = Ising(state)
+    obj.calculate_moments()
+    
+    specHeat = (obj.energy2 - np.square(obj.energy)) / T**2
+    susc = (obj.mag2 - np.square(obj.mag)) / T
+    
+    return np.array([obj.mag, obj.energy, susc, specHeat,
+                     obj.mag2 / obj.N_spins, obj.mag4 / obj.N_spins**3,
+                     obj.energy2 / obj.N_spins]) / obj.N_spins
+
+def get_observables_with_corr(state, T):
+    ## Returns [Mag, En, Susc, specHeat, Mag2, Mag4, En2, S0, S1, S2]
+    state = 2 * state - 1
+    obs = get_observables_assist(state, T)
+    corr = correlation_lengths(state)
+    return np.concatenate((obs, corr))
+
+def get_observables_with_tpf(state, T):
+    ## Returns [Mag, En, Susc, specHeat, Mag2, Mag4, En2, tpf(L/2), tpf(L/4)]
+    state = 2 * state - 1
+    L = state.shape[1]
+    
+    obs = get_observables_assist(state, T)
+    tpf1 = two_point_function(state, L//2)
+    tpf2 = two_point_function(state, L//4)
+    
+    return np.concatenate((obs, np.array([tpf1]), np.array([tpf2])))
+
+def get_observables_with_corr_and_tpf(state, T):
+    ## Returns [Mag, En, Susc, specHeat, Mag2, Mag4, En2, 
+    ## tpf(L/2), tpf(L/4), S0, S1, S2,]
+    state = 2 * state - 1
+    L = state.shape[1]
+    
+    obs = get_observables_assist(state, T)
+    corr = correlation_lengths(state)
+    tpf1 = two_point_function(state, L//2)
+    tpf2 = two_point_function(state, L//4)
+    
+    return np.concatenate((obs, np.array([tpf1]), np.array([tpf2]), corr))
+
+def get_moments_with_errors(state):
+    ## Returns array of [quantity, error] (5x2)
+    ## Quantities Mag, Mag2, Mag4, En, En2
+    obs = np.empty([5, 2])
+    obj = IsingErrors(2 * state - 1)
+    obj.calculate_moments()
+    
+    obs[0] = np.array([obj.mag, obj.errmag])
+    obs[1] = np.array([obj.mag2, obj.errmag2])
+    obs[2] = np.array([obj.mag4, obj.errmag4])
+    obs[3] = np.array([obj.energy, obj.errenergy])
+    obs[4] = np.array([obj.energy2, obj.errenergy2])
+    
+    return obs
+    
+
+##########################################
+########## CALCULATIONS CLASSES ##########
+##########################################
+
+class Ising():
+    def __init__(self, state):
+        (self.n_samples, self.Ly, self.Lx) = state.shape
+        self.N_spins = self.Lx * self.Ly
+        self.state = state
+        
+    def _calculate_magnetization(self):
+        self.sample_mag = np.sum(self.state, axis=(1,2))
+        
+    def _calculate_energy(self, Jx=1, Jy=1):
+        ## Returns total energy of the current state - Full calculation ##
+        # Energy from x interactions
+        Ex = np.sum(self.state[:,:,1:] * self.state[:,:,:self.Lx-1], axis=(1,2))
+        # Energy from y interactions
+        Ey = np.sum(self.state[:,1:,:] * self.state[:,:self.Ly-1,:], axis=(1,2))
+        
+        # Fix periodic boundary conditions
+        Ex+= np.sum(self.state[:,:,0] * self.state[:,:,self.Lx-1], axis=1)
+        Ey+= np.sum(self.state[:,0,:] * self.state[:,self.Ly-1,:], axis=1)
+        
+        self.sample_energy = - (Jx * Ex + Jy * Ey)
+    
+    def calculate_moments(self):
+        self._calculate_magnetization()
+        self.mag  = np.mean(np.abs(self.sample_mag))
+        self.mag2 = np.mean(np.square(self.sample_mag))
+        self.mag4 = np.mean(self.sample_mag**4)
+        
+        self._calculate_energy()
+        self.energy  = np.mean(self.sample_energy)
+        self.energy2 = np.mean(np.square(self.sample_energy))
+
+    
+class IsingErrors():
+    def __init__(self, state):
+        (n_samples, Ly, Lx) = state.shape
+        self.N_spins = Lx * Ly
+        self.state = state
+        
+    def average_and_std(self, x):
+        return np.mean(x), np.std(x)
+    
+    def _calculate_magnetization(self):
+        ## Fixed to calculate the magnetization of many samples of states simultaneously ##
+        ## Returns the average magnetization over all samples ##
+        self.sample_mag = np.mean(self.state, axis=(1,2))
+        
+    def _calculate_energy(self, Jx=1, Jy=1):
+        ## Returns total energy of the current state - Full calculation ##
+        # Energy from x interactions
+        Ex = np.sum(self.state[:,:,1:] * self.state[:,:,:self.Lx-1], axis=(1,2))
+        # Energy from y interactions
+        Ey = np.sum(self.state[:,1:,:] * self.state[:,:self.Ly-1,:], axis=(1,2))
+        
+        # Fix periodic boundary conditions
+        Ex+= np.sum(self.state[:,:,0] * self.state[:,:,-1], axis=1)
+        Ey+= np.sum(self.state[:,0,:] * self.state[:,-1,:], axis=1)
+        
+        self.sample_energy = - (Jx * Ex + Jy * Ey)
+    
+    def calculate_moments(self):
+        self._calculate_magnetization()
+        self.mag, self.errmag = self.average_and_std(np.abs(self.sample_mag))
+        self.mag2, self.errmag2 = self.average_and_std(np.square(self.sample_mag))
+        self.mag4, self.errmag4 = self.average_and_std(self.sample_mag**4)
+        
+        self._calculate_energy()
+        self.energy, self.errenergy = self.average_and_std(self.sample_energy)
+        self.energy2, self.errenergy2 = self.average_and_std(
+                np.square(self.sample_energy))
+        
+        #return np.array([self.mag, self.energy / self.N_spins, 
+        #                 self.N_spins * susc, specHeat / self.N_spins])
