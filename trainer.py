e80f88bdca515459a205b73765052f5ef6e9f9bb
stavros11 <35475381+stavros11@users.noreply.github.com>
2018-07-22 14:19:59 +0300

    Batch calculation for MSE and observables

--- trainer.py
+++ trainer.py
@@ -12,8 +12,14 @@ from ising import get_observables_with_corr_and_tpf
 
 class Trainer(ConvRBM_Train):
     def fit(self, data, T):
-        n_batches = data.shape[0] // self.args.BS
+        train_batches = data.shape[0] // self.args.BS
         obs_correct = get_observables_with_corr_and_tpf(data[:,:,:,0], T)
+        
+        # calculation batches (for memory)
+        mse_batches = data.shape[0] // self.args.BSC
+        mse_error = np.zeros(mse_batches)
+#        obs_batches = self.args.nTE // self.args.BSC
+#        obs_test = np.zeros([obs_batches, len(obs_correct)])
         
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=self.args.GPU)
         sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
@@ -22,33 +28,48 @@ class Trainer(ConvRBM_Train):
         ## Initialize lists to keep track of weights
         self.W_list, self.hid_bias_list, self.vis_bias_list = [], [], []
         self.update_weight_lists(sess)
+        ## Initialize learning rate
+        self.learning_rate = self.args.LR
         for iE in range(self.args.EP):
             np.random.shuffle(data)
-            for iB in range(n_batches):
+            for iB in range(train_batches):
                 sess.run(self.train_op, 
-                         feed_dict={self.visible : data[iB * self.args.BS : (iB+1) * self.args.BS]})
+                         feed_dict={self.visible : data[iB * self.args.BS : (iB+1) * self.args.BS],
+                                    self.learning_rate_plc : self.learning_rate})
             
             self.update_weight_lists(sess)
+            # Linear LR decay
+            self.learning_rate -= self.linear_lr_update
+            
             ## MSE Test
             if iE % self.args.MSG == 0:
-                v_recon = sess.run(self.v_gibbs_op, feed_dict={self.visible : data})
-                mse_error = np.mean(np.square(v_recon - data))
+                for iB in range(mse_batches):
+                    v_recon = sess.run(self.v_gibbs_recon_op, 
+                                       feed_dict={self.visible : data[iB * self.args.BSC : (iB+1) * self.args.BSC]})
+                    mse_error[iB] = np.mean(np.square(v_recon - 
+                             data[iB * self.args.BSC : (iB+1) * self.args.BSC]))
                 #v_rand = sess.run(v_gibbs, feed_dict={rbm.visible : np.random.randint(0, 2, data.shape)})
                 #mse_error_random = np.mean(np.square(v_rand - data))   ## GIVES 0.5 as it should
-                print('\n%d / %d epochs done!\nMSE: %.5E\n'%(iE+1, self.args.EP, mse_error))
+                print('\n%d / %d epochs done!\nMSE: %.5E\n'%(iE+1, self.args.EP, mse_error.mean()))
+                #print('\n%d / %d epochs done!\n'%(iE+1, self.args.EP))
+                
+                ## Add Free energy calculations here to track overfitting
             
             ## Observables Test
-            if iE % self.args.MSGC == 0:
-                #v_test = sess.run(v_gibbs, feed_dict={rbm.visible : np.random.randint(
-                #        0, 2, size=(args.nTE, args.L, args.L, 1))})[:,:,:,0]
-                v_test = sess.run(self.v_gibbs_rand_op)[:,:,:,0]
-                obs_test = get_observables_with_corr_and_tpf(v_test, T)
-                err_obs = np.abs(obs_test - obs_correct) * 100 / obs_correct
-                print(err_obs)
+#            if iE % self.args.MSGT == 0:
+#                #v_test = sess.run(v_gibbs, feed_dict={rbm.visible : np.random.randint(
+#                #        0, 2, size=(args.nTE, args.L, args.L, 1))})[:,:,:,0]
+#                for iB in range(obs_batches):
+#                    v_test = sess.run(self.v_gibbs_test_op)[:,:,:,0]
+#                    obs_test[iB] = get_observables_with_corr_and_tpf(v_test, T)
+#                err_obs = np.abs(obs_test.mean(axis=0) - obs_correct) * 100 / obs_correct
+#                print(err_obs)
             
             ## Weight averaging update
             if iE >= self.args.WAVEP and (iE - self.args.WAVEP) % self.args.WAVST == 0:
                 self.update_network_weights(sess)
+                
+        return self.W_list, self.hid_bias_list, self.vis_bias_list
 
     def update_weight_lists(self, sess):
         W_temp, hid_bias_temp, vis_bias_temp = sess.run([self.filter, self.hid_bias, self.vis_bias])
