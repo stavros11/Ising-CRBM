4e4d9c85e9450611ef8b56f4f73ef50e9879e965
stavros11 <35475381+stavros11@users.noreply.github.com>
2018-08-03 16:14:26 +0300

    Error label

--- plot.py
+++ plot.py
@@ -17,15 +17,15 @@ import matplotlib.pyplot as plt
 #plt.show()
 
 ## Observables ##
-kI, kF = 5, 2000
-obs = np.load('Observables/Critical/CRBML8_W8K64BS50LR0.00100_VER2_EP500/k%d_%d.npy'%(kI, kF))
+kI, kF = 5, 3000
+obs = np.load('Observables/Critical/CRBML8_W8K64BS50LR0.00100_VER1_EP500/k%d_%d.npy'%(kI, kF))
 
 quant_list = ['$M$', '$E$', '$\chi $', '$C_V$', '$M^2$', '$M^4$', '$E^2$',
               '$C(L/2)$', '$C(L/4)$', '$S_0$', '$S_1$', '$S_2$']
 
 errors = np.abs((obs[1:] - obs[0]) / obs[0]) * 100
 
-def plot_quantity(q=0, figsize=(10, 6)):
+def plot_quantity(q=0, figsize=(7, 4)):
     plt.figure(figsize=figsize)
     plt.plot(np.arange(kI, kF+1), obs[1:, q], color='blue')
     plt.axhline(y=obs[0, q], linestyle='--', color='k')
@@ -33,10 +33,10 @@ def plot_quantity(q=0, figsize=(10, 6)):
     plt.ylabel(quant_list[q], fontsize=20)
     plt.show()
     
-def plot_errors(q=0, figsize=(10, 6)):
+def plot_errors(q=0, figsize=(7, 4)):
     plt.figure(figsize=figsize)
     plt.plot(np.arange(kI, kF+1), errors.T[q], color='blue')
     plt.axhline(y=0, linestyle='--', color='k')
     plt.xlabel('$k$', fontsize=20)
-    plt.ylabel(quant_list[q], fontsize=20)
+    plt.ylabel(quant_list[q] + ' - Error', fontsize=20)
     plt.show()
