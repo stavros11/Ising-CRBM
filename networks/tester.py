639b9ae190394c0eb588f4d511354ef66931b894
stavros11 <35475381+stavros11@users.noreply.github.com>
2018-08-03 14:12:00 +0300

    Testing class

---
+++ networks/tester.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  3 13:10:58 2018
+
+@author: Stavros
+"""
+
+import numpy as np
+import tensorflow as tf
+from os import listdir
+from crbm import ConvRBM
+from ising import get_observables_with_corr_and_tpf
+
+class Tester(ConvRBM):
+    def __init__(self, args, T=0.0):
+        self.args = args
+        self.T = T
+        
+        save_dir = 'Trained_Models/%s'%['T%.4f'%T, 'Critical'][int(self.args.CR)]
+        self.name = listdir(save_dir)[self.args.Mind]
+        
+        save_dir += '/%s/'%self.name
+        
+        w = np.load(save_dir + 'trained_weights.npy')
+        hb = np.load(save_dir + 'trained_hid_bias.npy')
+        vb = np.load(save_dir + 'trained_vis_bias.npy')
+        
+        self.Nv = self.args.L
+        self.Nw, self.K = w.shape[0], w.shape[-1]
+        self.Nh = self.Nv - self.Nw + 1
+        
+        self.create_basic_parameters(w, hb, vb)
+        
+    def prepare_testing(self, data):
+        self.n_batches = self.args.nTE // self.args.BSC
+        
+        ## Create ops
+        self.init_op = self.create_gibbs_sampler_random(self.args.BSC, k=self.args.kI)
+        self.block_op = self.create_gibbs_sampler(k=1)
+        
+        ## Initialize observable list
+        self.obs_list = [get_observables_with_corr_and_tpf(data, self.T)]
+        self.n_observables = len(self.obs_list[0])
+        
+        ## Define Session
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=self.args.GPU)
+        self.sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+        self.sess.run(tf.global_variables_initializer())
+                
+    def predict_init(self):
+        obs_prediction = np.zeros([self.n_batches, self.n_observables])
+        pred_samples = np.zeros([self.args.nTE, self.Nv, self.Nv, 1])
+        for iB in range(self.n_batches):
+            pred_batch = self.sess.run(self.random_op)
+            
+            obs_prediction[iB] = get_observables_with_corr_and_tpf(pred_batch[:,:,:,0], self.T)
+            pred_samples[iB * self.args.BSC : (iB+1) * self.args.BSC] = np.copy(pred_batch)
+            
+        return pred_samples, obs_prediction.mean(axis=0)
+    
+    def predict_block(self, data_in):
+        obs_prediction = np.zeros([self.n_batches, self.n_observables])
+        pred_samples = np.zeros([self.args.nTE, self.Nv, self.Nv, 1])
+        for iB in range(self.n_batches):
+            pred_batch = self.sess.run(self.block_op, feed_dict={
+                    self.visible : data_in[iB * self.args.BSC : (iB+1) * self.args.BSC]})
+    
+            obs_prediction[iB] = get_observables_with_corr_and_tpf(pred_batch[:,:,:,0], self.T)
+            pred_samples[iB * self.args.BSC : (iB+1) * self.args.BSC] = np.copy(pred_batch)
+            
+        return pred_samples, obs_prediction.mean(axis=0)
+    
+    def test(self):
+        ## Initial prediction
+        samples, obs_temp = self.predict_init()
+        self.obs_list.append(obs_temp)
+        
+        ## Block predictions
+        for k in range(self.args.kI+1, self.args.kF+1):
+            samples, obs_temp = self.predict_block(samples)
+            self.obs_list.append(obs_temp)
+            
+            ## Print observables
+            print('\nCorrect vs Predicted Observables with k=%d:'%k)
+            for (cor, pred) in zip(self.obs_list[0], self.obs_list[-1]):
+                print('%.6f  -  %.6f'%(cor, pred))
+            print('\n')
+        
+        return np.array(self.obs_list)
+        
--- test_multiple_k.py
+++ test_multiple_k.py
@@ -6,10 +6,8 @@ Created on Sun Jul 29 19:05:48 2018
 """
 
 import numpy as np
-import tensorflow as tf
 import data.loaders as dl
-from networks.loader import get_model
-from networks.ising import get_observables_with_corr_and_tpf
+from networks.tester import Tester
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
@@ -20,10 +18,11 @@ parser.add_argument('-L', type=int, default=16, help='configuration size')
 
 ## Model parameters
 parser.add_argument('-Mind', type=int, default=0, help='model index')
-parser.add_argument('-GPU', type=float, default=0.5, help='GPU fraction')
+parser.add_argument('-GPU', type=float, default=0.4, help='GPU fraction')
 
 ## Test parameters
 parser.add_argument('-nTE', type=int, default=50000, help='RBM sampling test size')
+parser.add_argument('-BSC', type=int, default=5000, help='calculation batch size')
 parser.add_argument('-kI', type=int, default=5, help='Gibbs sampling k')
 parser.add_argument('-kF', type=int, default=40, help='Gibbs sampling k')
 
@@ -32,57 +31,30 @@ def main(args):
         critical_n_samples = {8 : 40000, 16 : 100000}
         T = 2 / np.log(1 + np.sqrt(2))
         
-        rbm, rbm_name = get_model(args.Mind, Nv=args.L, critical=True)
         data = dl.read_file_critical(L=args.L, n_samples=critical_n_samples[args.L], train=False)
         
     else:
         from data.directories import T_list
         T = T_list[args.iT]
         
-        rbm, rbm_name = get_model(args.Mind, Nv=args.L, critical=False, T=T)
         data = dl.temp_partition(dl.read_file(L=args.L, n_samples=10000, train=False),
                                  args.iT)
     
     
-    ## Initialize observable list
-    obs_list = [get_observables_with_corr_and_tpf(data, T)]
-    print('\nInitiating testing with %s.\n'%rbm_name)
+    ## Initialize
+    rbm = Tester(args, T=T)
+    print('\nInitiating testing with %s.\n'%rbm.name)
     
-    for k in range(args.kI, args.kF+1):
-        if k < 10:
-            BSC = 5000
-        elif k < 20:
-            BSC = 2000
-        else:
-            BSC = 1000
-        
-        ## Create ops
-        n_batches = args.nTE // BSC
-        obs_prediction = np.zeros([n_batches, len(obs_list[0])])
-        ops = rbm.create_gibbs_sampler_random(BSC, k=k)
-        
-        ## Define Session
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.GPU)
-        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-        sess.run(tf.global_variables_initializer())
-        
-        for iB in range(n_batches):
-            sampled = sess.run(ops)
-            obs_prediction[iB] = get_observables_with_corr_and_tpf(sampled[:,:,:,0], T)
-        
-        obs_list.append(obs_prediction.mean(axis=0))
+    ## Test
+    rbm.prepare_testing(data)
+    obs_list = rbm.test()
     
-        ## Print observables
-        print('\nCorrect vs Predicted Observables with k=%d:'%k)
-        for (cor, pred) in zip(obs_list[0], obs_list[-1]):
-            print('%.6f  -  %.6f'%(cor, pred))
-        print('\n')
-        
+    ## Save
     save_dir = 'Observables/%s'%['T%.4f'%T, 'Critical'][int(args.CR)]
     dl.create_directory(save_dir)
-    save_dir += '/%s'%rbm_name
+    save_dir += '/%s'%rbm.name
     dl.create_directory(save_dir)
-    np.save('%s/k%d_%d.npy'%(save_dir, args.kI, args.kF), np.array(obs_list))
+    np.save('%s/k%d_%d.npy'%(save_dir, args.kI, args.kF), obs_list)
     
     ## Saved .npy format: Array (different k (0 = correct), 12 observables)
     return
